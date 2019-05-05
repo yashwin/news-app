ddf6137fbf23b06f7862dc4d1a8f8fb32102346f
Yashwin <yashwin@Yashwins-MacBook-Air.local>
2019-05-05 18:30:50 +0530

    feat: add links to each news

--- src/components/NewsDetail/NewsDetail.jsx
+++ src/components/NewsDetail/NewsDetail.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
-import './NewsDetail.css'
+import './NewsDetail.css';
 
 export default function NewsDetail(props) {
   const news = props.news;
   return (
-    <div className="JustifyCenter">
-      <div className="card mb-3 col-md-4">
-        <div className="row no-gutters">
-          <div className="col-md-4 MarginAuto">
-            <img src={news.thumbnail} className="card-img" alt={news.headline} />
-          </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h6 className="card-title">{news.headline}</h6>
-              {
-                news.keywords.map((keyword) => (
-                  <div className="Tags" key={keyword.id}>
-                    {keyword.webTitle}
-                  </div>
-                ))
-              }
+    <div class="col-md-5 MarginAuto">
+      <a href={news.url} target='_blank' rel="noopener noreferrer" className="JustifyCenter CardLink">
+        <div className="card mb-3 Card">
+          <div className="row no-gutters">
+            <div className="col-md-4 MarginAuto">
+              <img src={news.thumbnail} className="card-img" alt={news.headline} />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h6 className="card-title">{news.headline}</h6>
+                {
+                  news.keywords.map((keyword) => (
+                    <div className="Tags" key={keyword.id}>
+                      {keyword.webTitle}
+                    </div>
+                  ))
+                }
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </a>
     </div>
   )
 }
